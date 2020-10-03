52ffd990bc1e24fb762144105b15b71ac9f8eab7
gpellicci <pellicci.giac@gmail.com>
2020-10-03 10:52:53 +0200

    added host interface (socket to communicate with qemu device)

---
+++ host_interface/propagateHeaders.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Copy to qemu tree to let it compile
+cp /home/giacomo/socket_test/bpf_injection_msg.h /home/giacomo/Desktop/tesi/qemu/hw/misc/
+
+# Send to guest code (driver)
+# Cut portions that implements userspace functions or require userspace libraries
+sed -n '/\/\/cut/,/\/\/cut/{/\/\/cut/!{/\/\/cut/!p}}' /home/giacomo/socket_test/bpf_injection_msg.h > /home/giacomo/myvm/shared/driver/bpf_injection_msg.h
+
+# Copy to guest daemon directory
+cp /home/giacomo/socket_test/bpf_injection_msg.h /home/giacomo/myvm/shared/daemon_bpf/bpf_injection_msg.h
+
+
