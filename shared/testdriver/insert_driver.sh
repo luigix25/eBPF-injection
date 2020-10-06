66b2104550e147d84af9783853d0fb447757f854
gpellicci <pellicci.giac@gmail.com>
2020-10-06 16:41:50 +0200

    removed unused files

--- shared/testdriver/insert_driver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-# Setup. (ins kernel module, create device file at /dev/newdev)
-insmod driver.ko
-./mknoddev.sh newdev
--- shared/testdriver/remove_driver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-# Teardown.
-rm /dev/newdev
-rmmod driver
