223471e1f8d1a76a0baa4acc694088ab71b2d310
gpellicci <pellicci.giac@gmail.com>
2020-10-09 18:09:48 +0200

    updated path name

--- buildubuntu.sh
+++ buildubuntu.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # Parameters.
-id=ubuntu-20.04-live-server-amd64 #ubuntu-18.04.1-desktop-amd64
+id=ubuntu-20.04.1-live-server-amd64 #ubuntu-18.04.1-desktop-amd64
 disk_img="${id}.img.qcow2"
 disk_img_snapshot="${id}.snapshot.qcow2"
 iso="${id}.iso"
--- host_interface/propagateHeaders.sh
+++ host_interface/propagateHeaders.sh
@@ -3,13 +3,13 @@
 set -eux
 
 # Copy to qemu tree to let it compile
-cp /home/giacomo/myvm/host_interface/bpf_injection_msg.h /home/giacomo/Desktop/tesi/qemu/hw/misc/
+cp /home/giacomo/eBPF-injection/host_interface/bpf_injection_msg.h /home/giacomo/Desktop/tesi/qemu/hw/misc/
 
 # Send to guest code (driver)
 # Cut portions that implements userspace functions or require userspace libraries
-sed -n '/\/\/cut/,/\/\/cut/{/\/\/cut/!{/\/\/cut/!p}}' /home/giacomo/myvm/host_interface/bpf_injection_msg.h > /home/giacomo/myvm/shared/driver/bpf_injection_msg.h
+sed -n '/\/\/cut/,/\/\/cut/{/\/\/cut/!{/\/\/cut/!p}}' /home/giacomo/eBPF-injection/host_interface/bpf_injection_msg.h > /home/giacomo/eBPF-injection/shared/driver/bpf_injection_msg.h
 
 # Copy to guest daemon directory
-cp /home/giacomo/myvm/host_interface/bpf_injection_msg.h /home/giacomo/myvm/shared/daemon_bpf/bpf_injection_msg.h
+cp /home/giacomo/eBPF-injection/host_interface/bpf_injection_msg.h /home/giacomo/eBPF-injection/shared/daemon_bpf/bpf_injection_msg.h
 
 
