2087c250670b3af6f3595ab0e3167bff883372d2
gpellicci <pellicci.giac@gmail.com>
2020-10-09 17:31:16 +0200

    renewed shared

--- shared/simplified-spscq/busycpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-taskset 0x00000001 yes > /dev/null &
-taskset 0x00000002 yes > /dev/null &
-taskset 0x00000004 yes > /dev/null &
-taskset 0x00000008 yes > /dev/null &
-
-echo "CPU 0, 1, 2, 3     busy with yes"
--- shared/simplified-spscq/clearcpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pkill -f yes
-
-echo "CPU 0, 1, 2, 3     clear"
